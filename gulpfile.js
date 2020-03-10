e3f8df91f9e8517b77242a8853b460c400a9484c
Milan <milancakic89@gmail.com>
2020-03-10 18:04:56 +0100

    Installing modules for automation, and creating folder structure

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+var gulp = require("gulp");
+var watch = require("gulp-watch");
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssvars = require('postcss-simple-vars');
+var nested = require("postcss-nested");
+var cssImport = require('postcss-import');
+var browserSync = require("browser-sync");
+
+gulp.task("default", function(){
+    console.log("hooray - you just created a gulp task");
+});
+gulp.task("html", function(){
+    console.log('something great just happened');
+})
+gulp.task("styles", function(){
+   return gulp.src("./app/assets/styles/styles.css")
+   .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+   .pipe(gulp.dest("./app/temp/styles"));
+})
+
+
+gulp.task("watch", function(){
+browserSync.init({
+    notify: false,
+    server:{
+        baseDir: "app"
+    }
+})
+
+    watch('./app/index.html', function(){
+        browserSync.reload()
+    })
+    watch('./app/new-portfolio.html', function(){
+        browserSync.reload()
+    })
+    watch('./app/contact.html', function(){
+        browserSync.reload()
+    })
+    watch('./app/portfolio.html', function(){
+        browserSync.reload()
+    })
+    watch("./app/assets/styles/**/*.css", function(){
+        gulp.start('cssInject')
+    })
+})
+
+gulp.task("cssInject", ["styles"], function(){
+    return gulp.src("app/temp/styles/styles.css")
+    .pipe(browserSync.stream())
+})
