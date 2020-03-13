2c242978a8fbc8266e5345d882286a3510c50308
Milan <milancakic89@gmail.com>
2020-03-13 16:08:18 +0100

    code refatored to more files

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,55 +1,22 @@
-const Game = require('./includes/_Game');
-const Questions = require('./includes/_Questions');
-const LS = require('./includes/_LS');
-const ResultButtons = require('./includes/_ResultButtons');
+const Game = require('./includes/Game');
+const Questions = require('./includes/Questions');
+const LS = require('./includes/LS');
+const ResultButtons = require('./includes/ResultButtons');
+const Elements = require('./includes/Elements');
 
 const newGame = new Game();
 
 let questionsList = null;
 
-
-let homeScreen = document.getElementById('home-screen');
-let mainScreen = document.getElementById('startscreen');
-let questionScreen = document.getElementById('question-screen');
-let resultScreen = document.querySelector('.result__screen');
-
-let buttonPlay = document.getElementById('play');
-let buttonPlayAgain = document.getElementById('play-again');
-let buttonBackToStart = document.getElementById('back-to-main');
-let homeButtonPlay = document.getElementById('home-button-play');
-let buttonGoToHome = document.getElementById('go-to-home');
-
-let buttonOne = document.querySelector('.buttonOne');
-let buttonTwo = document.querySelector('.buttonTwo');
-let buttonThree = document.querySelector('.buttonThree');
-let buttonNext = document.querySelector('.buttonNext');
-
-let questionCounter = document.getElementById('question-count');
-let img = document.getElementById('image-url');
-
-
-/*
-    NOTE: I HAVE DESIDED TO MAKE ENTIRE GAME TROUGHT JS, I COULD USE EASIER APROACH
-          WITH MORE HTML AND CSS, AND LESS JS... PRACTICING VANILA JS IS MY MAIN GOAL HERE,
-          AND I WOULD APPRITIATE NOTICE ON ANY MISTAKE I MADE.
-*/
-buttonGoToHome.addEventListener('click', ResultButtons.goToHome);
-homeButtonPlay.addEventListener('click', ResultButtons.jumpToLedderboard);
-
-
-
+Elements.buttonGoToHome.addEventListener('click', ResultButtons.goToHome);
+Elements.homeButtonPlay.addEventListener('click', ResultButtons.jumpToLedderboard);
 
 
 // fetching questions
-Questions.getQuestions()
-.then(result=>{
-    // storing questions
-    questionsList = result;
-   //- newQuestions.setQuestions(result);
-});
+Questions.getQuestions().then(result => questionsList = result).catch(err => console.log(err));
 
 //adding an event listener to play button at start
-buttonPlay.addEventListener('click', letsPlay);
+Elements.buttonPlay.addEventListener('click', letsPlay);
 
 function letsPlay(){
     //checking if the questions are stored
@@ -61,19 +28,18 @@ function letsPlay(){
 
 // function for creating correct answer and two random wrong answers
 function getQuestionsList(){
-     buttonOne.disabled = false;
-     buttonTwo.disabled = false;
-     buttonThree.disabled = false;
-     buttonOne.className = 'question__button buttonOne';
-     buttonTwo.className = 'question__button buttonTwo';
-     buttonThree.className = 'question__button buttonThree';
-     buttonNext.className = 'question__next buttonNext hidden';
+    //this function is called on every question, question needs to be enabled
+    //because they are all disabled once the user clicks on any button
+     Elements.buttonOne.disabled = false;
+     Elements.buttonTwo.disabled = false;
+     Elements.buttonThree.disabled = false;
+     Elements.buttonOne.className = 'question__button buttonOne';
+     Elements.buttonTwo.className = 'question__button buttonTwo';
+     Elements.buttonThree.className = 'question__button buttonThree';
+     Elements.buttonNext.className = 'question__next buttonNext hidden';
 
-    //since this function will be called more than once, making sure game is not over
+    //making sure game is not over
     if(newGame.isOver() !== true && newGame.getCounter() !==5){
-
-        //storing all questions in variable
-        
 
         //array for 2 wrong questions
         let wrongAnswers = [];
@@ -105,37 +71,48 @@ function getQuestionsList(){
         //we have one correct, and two wrong question, we can move on
         renderQuestion(correctAnswer, wrongAnswers);
     }else{
+        //game over, display results and save score to local storage
+        Elements.questionScreen.classList.add('hidden');
+        Elements.resultScreen.classList.remove('hidden');
+        Elements.document.getElementById('score-container').textContent = newGame.getScore();
 
-        questionScreen.classList.add('hidden');
-        resultScreen.classList.remove('hidden');
-        document.getElementById('score-container').textContent = newGame.getScore();
+        Elements.buttonPlayAgain.addEventListener('click', letsPlayAgain);
+        Elements.buttonBackToStart.addEventListener('click', backToStart);
 
-        buttonPlayAgain.addEventListener('click', letsPlayAgain);
-        buttonBackToStart.addEventListener('click', backToStart);
+        let scoreToLocalStorage = newGame.getScore();
 
-        let scoreToLs = newGame.getScore();
-
-        LS.setLocalStorage(scoreToLs);
+        LS.setLocalStorage(scoreToLocalStorage);
     }
 }
+//function to go back from result screen to main screen
 function backToStart(){
     
     newGame.resetGame();
-    resultScreen.classList.add('hidden');
-    mainScreen.classList.remove('hidden');
-    
-    LS.getLocalStorage();
-    
-   
+    Elements.resultScreen.classList.add('hidden');
+    Elements.mainScreen.classList.remove('hidden');
+    LS.getLocalStorage(); 
 }
+//function to directly go from result page to a new game
 function letsPlayAgain(){
     newGame.resetGame();
     getQuestionsList();
-    questionScreen.classList.remove('hidden');
-    resultScreen.classList.add('hidden');
+    Elements.questionScreen.classList.remove('hidden');
+    Elements.resultScreen.classList.add('hidden');
 }
-//
+//responsible for creating questions and injecting values into buttons
 function renderQuestion(correctAnswer, wrongAnswers){
+    /*
+        we have questions we need.
+        We cant just set values in buttons, the answer would be allways on same spot.
+        We need to mix them randomly and inject in button values.
+        idea: 
+        Put all three answers into array. Pick random 1 from 3 and put him into variable.
+        Thats the first random question pick. Now we need to remove that question from an array,
+        we dont want that same question as an option. So filter array to remove that question, 
+        now we have array with 2, random again bettwen those 2, assign it to variable, thats second random question.
+        Filter again, remaining question is the third question... Now we append one by one, and we dont know
+        witch one is actualy correct.
+    */
     
     let questionListWithThree = [correctAnswer, wrongAnswers[0], wrongAnswers[1]];
     let firstContinent = questionListWithThree[Math.round(Math.random()*2)] 
@@ -149,81 +126,57 @@ function renderQuestion(correctAnswer, wrongAnswers){
     //current question to be displayed;
     let questionOnScreen = newGame.getCounter();
 
-    //clean screen, this will be called on each question, we clean previous, then we render next
     //screen is cleaned, we can now render question
-    newGame.updateCounter();
+     newGame.updateCounter();
 
     //current question on screen
-    questionOnScreen = newGame.getCounter();
+     questionOnScreen = newGame.getCounter();
    
     //adding current question value to be displayed
-    questionCounter.textContent = `Question: ${questionOnScreen}`;
+     questionCounter.textContent = `Question: ${questionOnScreen}`;
 
-    img.src = correctAnswer.image;
+    //displaying image on screen
+     img.src = correctAnswer.image;
 
-    mainScreen.classList.add('hidden');
-    questionScreen.classList.remove('hidden');
-
-    /*
-        We cant just append buttons now, the answer would be allways on same spot.
-        We need to mix them randomly.
-        idea: 
-        Put all answers into array. Pick random 1 from 3 and put him into variable,
-        thats the first random question pick. Now we need to remove that question from array,
-        we have it in variable, and for the next pick, we dont want that same question as
-        an option. So filter array to remove that question, now we have array with 2, random again
-        bettwen those 2, assign it to variable, thats second random question. Filter again,
-        remaining question is the third question... Now we append one by one, and we dont know
-        witch one is actualy correct.
-    */
-
+     Elements.mainScreen.classList.add('hidden');
+     Elements.questionScreen.classList.remove('hidden');
 
     //adding continents as values
-    buttonOne.value = firstContinent.continent;
-    buttonTwo.value = secondContinent.continent;
-    buttonThree.value = thirdContinent.continent;
+     Elements.buttonOne.value = firstContinent.continent;
+     Elements.buttonTwo.value = secondContinent.continent;
+     Elements.buttonThree.value = thirdContinent.continent;
 
     //this fields will help later with css when we click on button
-    buttonOne.id = firstContinent.continent;
-    buttonTwo.id = secondContinent.continent;
-    buttonThree.id = thirdContinent.continent;
+     Elements.buttonOne.id = firstContinent.continent;
+     Elements.buttonTwo.id = secondContinent.continent;
+     Elements.buttonThree.id = thirdContinent.continent;
     
     //adding text to buttons
-    buttonOne.textContent = firstContinent.continent;
-    buttonTwo.textContent = secondContinent.continent;
-    buttonThree.textContent = thirdContinent.continent;
+     Elements.buttonOne.textContent = firstContinent.continent;
+     Elements.buttonTwo.textContent = secondContinent.continent;
+     Elements.buttonThree.textContent = thirdContinent.continent;
 
     //adding event listeners
-    buttonOne.addEventListener('click', checkAnswer);
-    buttonTwo.addEventListener('click', checkAnswer);
-    buttonThree.addEventListener('click', checkAnswer);
-
+     Elements.buttonOne.addEventListener('click', checkAnswer);
+     Elements.buttonTwo.addEventListener('click', checkAnswer);
+     Elements.buttonThree.addEventListener('click', checkAnswer);
 }
-
-
-
 function checkAnswer(e){
-
 
    let picked = e.target.value;
    let correct = newGame.getCorrectAnswer();
 
-   let gotIt;
+   let gotCorrectAnswer;
    //if we got correct answer, that button will be displayed green
    //but if we not, we need to make button red, but also correct green
    //we will use this variable to check for that
-
    if(picked === correct){
        newGame.updateScore(750);
-       gotIt = true;
+       gotCorrectAnswer = true;
    }
-
-   //creating button NEXT
-
-   //assigning the same function as play at the beggining to event listener-
-   //we updated counter and score if needed, we will start same process again
-   buttonNext.addEventListener('click', getQuestionsList);
-   buttonNext.classList.remove('hidden')
+   //displaying button NEXT and adding event listener
+   Elements.buttonNext.addEventListener('click', getQuestionsList);
+   Elements.buttonNext.classList.remove('hidden')
 
    /*
     here is why i created ids on those buttons before.
@@ -231,16 +184,20 @@ function checkAnswer(e){
    let a = document.getElementById(`${picked}`);
    let b = document.getElementById(`${correct}`);
 
-   if(gotIt){
+   //if we have correct answer, both above variables will target same element
+   //if we have wrong answer, above variables will target diferent element
+   if(gotCorrectAnswer){
+       //adding class correct to correct button, displaying green
         b.classList.add('correct-answer')
-
    }else{
+       //adding class correct to correct button, and class wrong to wrong button
+       //displaying one green, one red
         a.classList.add('wrong-answer');
         b.classList.add('correct-answer')
 
    }
-   buttonOne.disabled = true;
-   buttonTwo.disabled = true;
-   buttonThree.disabled = true;
+   Elements.buttonOne.disabled = true;
+   Elements.buttonTwo.disabled = true;
+   Elements.buttonThree.disabled = true;
 
 }
---
+++ app/assets/js/includes/Elements.js
@@ -0,0 +1,19 @@
+exports.homeScreen = document.getElementById('home-screen');
+exports.mainScreen = document.getElementById('startscreen');
+exports.questionScreen = document.getElementById('question-screen');
+exports.resultScreen = document.querySelector('.result__screen');
+
+exports.buttonPlay = document.getElementById('play');
+exports.buttonPlayAgain = document.getElementById('play-again');
+exports.buttonBackToStart = document.getElementById('back-to-main');
+exports.homeButtonPlay = document.getElementById('home-button-play');
+exports.buttonGoToHome = document.getElementById('go-to-home');
+
+exports.buttonOne = document.querySelector('.buttonOne');
+exports.buttonTwo = document.querySelector('.buttonTwo');
+exports.buttonThree = document.querySelector('.buttonThree');
+exports.buttonNext = document.querySelector('.buttonNext');
+
+exports.questionCounter = document.getElementById('question-count');
+exports.scoreContainer = document.getElementById('score-container');
+exports.img = document.getElementById('image-url');
